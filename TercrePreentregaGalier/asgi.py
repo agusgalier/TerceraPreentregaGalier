8b824b010e414ae4f090d1c3e5657f5f442122f2
Agustin Galier <agusstingalier@gmail.com>
2023-06-27 01:28:55 -0300

    Arranco prentrega

---
+++ TercrePreentregaGalier/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for TercrePreentregaGalier project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TercrePreentregaGalier.settings')
+
+application = get_asgi_application()
