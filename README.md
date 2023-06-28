9ee34f669160c59e57488f1584d56c6a82eb2887
Agustin Galier <agusstingalier@gmail.com>
2023-06-28 20:04:08 -0300

    Version final

--- README.md
+++ README.md
@@ -4,42 +4,32 @@ Este el proyecto correspondiente a la tercera entrega del curso donde se debe de
 
 Descripción
  
- En la pagina desarolllada tenemos un la parte superior una barra de navegacion con las opciones de "Inicio","Futbolistas","Basquetbolistas","Tenistas", donde 
+ En la pagina desarolllada tenemos un la parte superior una barra de navegacion con las opciones de "Inicio","Futbolistas","Basquetbolistas","Tenistas", donde si seleccionamos "Inicio" nos mostrara un mensaje de bienvida y explica la funcion de la pagina.
+La opcion "Fubolistas" nos permite crear o buscar un futbolista en la base de datos previamente creado.
+La opcion "Basquetbolista" nos permite crear o buscar un basquetbolista en la base de datos previamente creado. 
+La opcion "Tenista" nos permite crear o buscar un tenista en la base de datos previamente creado.  
 
 
 Requisitos del Sistema
-Indica los requisitos necesarios para ejecutar tu página diseñada con Django, como la versión de Python y las dependencias adicionales. Por ejemplo:
 
-Python 3.7 o superior
-Django 3.2.5
-Otras dependencias y bibliotecas (si las hay)
+asgiref==3.7.2
+Django==4.2.2
+sqlparse==0.4.4
+tzdata==2023.3
+
 Instalación
-Proporciona instrucciones paso a paso sobre cómo instalar y configurar el proyecto en un entorno local. Incluye los comandos necesarios para clonar el repositorio, instalar las dependencias y configurar cualquier configuración adicional.
 
-bash
-Copy code
-git clone https://github.com/tu-usuario/tu-proyecto.git
-cd tu-proyecto
+Es necesarion insttalr requirements.txt para eso abrimos nuestra terminal y ejecutamos lo siguiente:
 pip install -r requirements.txt
-# Otros comandos de configuración (si los hay)
-Uso
-Explica cómo utilizar tu página diseñada con Django. Incluye los pasos necesarios para ejecutar el proyecto y cualquier información adicional que pueda ser útil para los usuarios. Por ejemplo:
+ 
+Acceso a la pagina web
 
-bash
-Copy code
+Para acceder a la pagina web desarollada una vez instalado requirements.txt abrimos nuestra terminal y ejecutamos lo siguiente:
 python manage.py runserver
 Luego, abre tu navegador y visita http://localhost:8000 para ver la página.
 
-Contribución
-Indica si aceptas contribuciones al proyecto y cómo los desarrolladores pueden contribuir. Puedes proporcionar instrucciones sobre cómo enviar solicitudes de extracción o informar problemas.
+Autor:Agustin Galier
 
-Autor
-Menciona quién es el autor o los autores del proyecto.
+Contacto:agusstingalier@gmail.com
 
-Licencia
-Indica la licencia bajo la cual se distribuye tu proyecto. Por ejemplo, puedes utilizar la Licencia MIT, Licencia Apache, etc.
-
-Contacto
-Proporciona información de contacto para que los usuarios puedan comunicarse contigo en caso de tener preguntas, comentarios o problemas relacionados con tu proyecto.
-
-¡Recuerda personalizar y completar la información en cada sección según las características específicas de tu proyecto! Espero que esta estructura básica te sea útil para crear tu archivo README. Si tienes alguna otra pregunta, no dudes en preguntar.
+¡
--- inicio/views.py
+++ inicio/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
+
 from inicio.forms import CrearFutbolistaFormulario, BuscarFutbolista, CrearBasquetbolistaFormulario, BuscarBasquetbolista, CrearTenistaFormulario,BuscarTenista
 from inicio.models import Futbolista, Basquetbolista, Tenista
 
@@ -7,22 +9,23 @@ from inicio.models import Futbolista, Basquetbolista, Tenista
 def inicio(request):
     return render(request, 'inicio/inicio.html')
 
+
+
 def crear_futbolista(request):
-    messages=''
-    if request.method =='POST':
-        formulario=CrearFutbolistaFormulario(request.POST)
+    if request.method == 'POST':
+        formulario = CrearFutbolistaFormulario(request.POST)
         if formulario.is_valid():
-            info=formulario.cleaned_data
-            futbolista=Futbolista(nombre=info['nombre'],edad=info['edad'],posicion=info['posicion'])
+            info = formulario.cleaned_data
+            futbolista = Futbolista(nombre=info['nombre'], edad=info['edad'], posicion=info['posicion'])
             futbolista.save()
-            # mensaje=f'Se creo el futbolista {futbolista.nombre}'
-            messages=messages.success(request, 'El jugador se ha creado exitosamente.')
+            messages.success(request, f'El jugador {futbolista.nombre} se ha creado exitosamente.')
             return redirect('inicio:lista_futbolistas')
         else:
-            # return render(request, 'inicio/crear_futbolista.html',{'formulario':formulario})
-            return render(request, 'inicio/crear_futbolista.html',{'formulario':formulario})
-    formulario=CrearFutbolistaFormulario()
-    return render(request, 'inicio/crear_futbolista.html',{'formulario':formulario,'mensaje':messages})
+            return render(request, 'inicio/crear_futbolista.html', {'formulario': formulario})
+    formulario = CrearFutbolistaFormulario()
+    return render(request, 'inicio/crear_futbolista.html', {'formulario': formulario})
+
+
 
 def lista_futbolistas(request):
    formulario=BuscarFutbolista(request.GET)
@@ -35,19 +38,18 @@ def lista_futbolistas(request):
    return render(request, 'inicio/lista_futbolistas.html',{'formulario':formulario,'futbolistas':lista_futbolistas}) 
 
 def crear_basquetbolista(request):
-    mensaje=''
     if request.method =='POST':
         formulario=CrearBasquetbolistaFormulario(request.POST)
         if formulario.is_valid():
             info=formulario.cleaned_data
             basquetbolista=Basquetbolista(nombre=info['nombre'],edad=info['edad'],posicion=info['posicion'])
             basquetbolista.save()
-            mensaje=f'Se creo el basquetbolista {basquetbolista.nombre}'
+            messages.success(request, f'El jugador {basquetbolista.nombre} se ha creado exitosamente.')
             return redirect('inicio:lista_basquetbolistas')
         else:
             return render(request, 'inicio/crear_basquetbolista.html',{'formulario':formulario})
     formulario=CrearBasquetbolistaFormulario()
-    return render(request, 'inicio/crear_basquetbolista.html',{'formulario':formulario,'mensaje':mensaje})
+    return render(request, 'inicio/crear_basquetbolista.html',{'formulario':formulario})
 
 def lista_basquetbolistas(request):
    formulario=BuscarBasquetbolista(request.GET)
@@ -67,12 +69,12 @@ def crear_tenista(request):
             info=formulario.cleaned_data
             tenista=Tenista(nombre=info['nombre'],edad=info['edad'],ranking=info['ranking'])
             tenista.save()
-            mensaje=f'Se creo el tenista {tenista.nombre}'
+            messages.success(request, f'El jugador {tenista.nombre} se ha creado exitosamente.')
             return redirect('inicio:lista_tenistas')
         else:
             return render(request, 'inicio/crear_tenista.html',{'formulario':formulario})
     formulario=CrearTenistaFormulario()
-    return render(request, 'inicio/crear_tenista.html',{'formulario':formulario,'mensaje':mensaje})
+    return render(request, 'inicio/crear_tenista.html',{'formulario':formulario})
 
 def lista_tenistas(request):
    formulario=BuscarTenista(request.GET)
